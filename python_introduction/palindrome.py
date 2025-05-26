da1818ee4550ac8e14c2957e66d22971c5bd1fdd
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-05-26 20:15:33 +0000

    added the ternary operator for palindrome check

--- python_introduction/palindrome.py
+++ python_introduction/palindrome.py
@@ -1,5 +1,10 @@
-word = input("Enter a word: ").lower()
-if word == word[::-1]:
+word = input("Enter a word: ").lower() #this converts the input to lowercase for case-insensitive
+if word == word[::-1]: #this reverses the string and checks if it's the same as the original
     print("The word is a palindrome")
+
 else:
     print("The word is not a palindrome")
+
+#ternary operator version
+message = "The word is a palindrome" if word ==word[::-1] else "The word is not a palindrome"
+print(message)
