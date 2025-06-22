79ec5188278cf46f815bffd7fcbc014f7c88a82e
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-06-22 16:10:37 +0000

    just because I didn't use else if in my code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,11 +4,18 @@ def perform_operation(num1, num2, operation):
             return num1 + num2
         case 'subtract':
             return num1 - num2
-        case 'divide': 
-            while num2 == 0:
-                print(f"{num2} cannot divide {num1}. Please enter a significant number...")
-                num2 = float(input("Reenter second number: "))
-            return num1 / num2
+        case 'divide':
+            if num2 != 0:
+                return num1 / num2
+            elif num2 == 0:
+                print(f"{num2} cannot divide {num1}")
+                num2 = float(input("Re-enter second number"))
+                return num1 / num2
+            else: print("check numbers entered well")
+            # while num2 == 0:
+            #     print(f"{num2} cannot divide {num1}. Please enter a significant number...")
+            #     num2 = float(input("Reenter second number: "))
+            # return num1 / num2
         case 'multiply':
             return num1 * num2
         case _:
