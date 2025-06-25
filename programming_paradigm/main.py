196434c6ee094059d11c0817a721d7e2bfd4cc57
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-06-25 21:46:18 +0000

    fixed the bug

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -9,8 +9,8 @@ def main():
     numerator = sys.argv[1]
     denominator = sys.argv[2]
 
-    safe_divide(numerator, denominator)
-    # print(result)
+    result = safe_divide(numerator, denominator)
+    print(result)
 
 if __name__ == "__main__":
     main()
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide(numerator, denominator):
     try:
         result = float(numerator) / float(denominator)
-        print("The result of the division is", result)  # ALX expects this
+        return f"The result of the division is {result}"  # ALX expects this
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
     except ValueError:
