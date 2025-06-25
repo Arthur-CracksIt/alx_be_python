3c648b4b19d31143e7d82c77d82ef4056100fcf9
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-06-25 21:48:03 +0000

    fixed the bug

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator):
         result = float(numerator) / float(denominator)
         return f"The result of the division is {result}"  # ALX expects this
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        return f"Error: Cannot divide by zero."
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return f"Error: Please enter numeric values only."
 
