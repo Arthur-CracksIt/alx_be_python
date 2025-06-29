5013ecc9ca84d695357c5fdd674766645d401d23
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-06-29 22:58:42 +0000

    create library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,21 +4,21 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"Title: {self.title}, author: {self.author}"
+        return f"Book: {self.title}, author: {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f"Title: {self.title}, author: {self.author}, file_size: {self.file_size}"
+        return f"EBook: {self.title}, author: {self.author}, file_size: {self.file_size}"
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
-        return f"Title: {self.title}, author: {self.author}, page_count: {self.page_count}"
+        return f"PrintBook: {self.title}, author: {self.author}, page_count: {self.page_count}"
 
 class Library:
     def __init__(self):
