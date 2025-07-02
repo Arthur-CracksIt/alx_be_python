d283ab250b94e94ba2a7af6d58d5c9a8e2170415
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-07-02 21:01:22 +0000

    sql learning

---
+++ oop/demo.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+db = mysql.connector.connect(host= "localhost", user= "root", passwd= "Protocol11#@ip.com", database = "Students")
+
+# print(db.server_info)
+
+mycursor = db.cursor() #to enable you scroll over the database/ walk through your database
+
+# mycursor.execute("select * from Students")
+# result = mycursor.fetchall()
+# for i in result:
+#     print(i)
+
+# mycursor.close()
+# db.close()
+
+#running my hands on it
+# script = "INSERT INTO Students(StudentId, firstName, secondName, email, hometown, age) Values(%s, %s, %s, %s, %s, %s)"
+# values = ('21123458', 'Emmanuel', 'Kwarko', 'emma.3@hotmail.com', 'bogoso', 23)
+
+# update = "UPDATE Students set firstName = %s where StudentId = %s"
+# # val = ("Percy", '20750963')
+# # mycursor.execute(update, val)
+# # db.commit()
+query = "select * from Students where StudentId = %s"
+value = '20750963',
+mycursor.execute(query, value)
+result = mycursor.fetchall()
+for i in result:
+    print(i)
+# db.commit()
+
+
+
